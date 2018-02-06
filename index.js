f44e1896639cb582191b5d079143e31edff1fd94
Joseph Surin <joseph.surin@gmail.com>
2018-02-06 17:22:37 +0800

    boilerplate

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const electron = require('electron')
-const { app, BrowserWindow } = electron
+const electron = require('electron');
+const { app, BrowserWindow } = electron;
 
-require('electron-reload')(__dirname, {})
+require('electron-reload')(__dirname);
 
-let mainWindow
+let mainWindow;
 
 app.on('ready', () => {
-    let mainWindow = new BrowserWindow({ width: 800, height: 600 })
-    mainWindow.setTitle('colour-game')
-    mainWindow.loadURL('file://'+__dirname+'/app/index.html')
-})
+    let mainWindow = new BrowserWindow({ width: 800, height: 600 });
+    mainWindow.loadURL('file://'+__dirname+'/app/index.html');
+});
